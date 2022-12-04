7ec46f141827f6ba5f8435474ef985fac2f541d6
tkhsmasa <115441873+tkhsmasa@users.noreply.github.com>
2022-12-04 23:08:35 +0000

    [add]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: "homes#top"
 
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
 
   get "/homes/about" => "homes#about", as: "about"
 
